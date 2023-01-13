5048ab7bdb109a2cc2dca59b7680801b309db33e
ctruon <89404069+ctruon@users.noreply.github.com>
2023-01-13 14:09:49 -0500

    Update SearchResults.js

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -4,20 +4,6 @@ import HelpArticleCard from "../cards/HelpArticleCard";
 import AuthorCard from "../cards/AuthorCard";
 import GuideCard from "../cards/GuideCard";
 
-// const handleSearch = (searchEventData) => {
-//   const { query } = searchEventData;
-//   const queryParams = new URLSearchParams(window.location.search);
-
-//   if (query) {
-//     queryParams.set("query", query);
-//   } else {
-//     queryParams.delete("query");
-//   }
-//   window.location.href = '/search?' + queryParams.toString();
-
-//   searchActions.executeUniversalQuery();
-// };
-
 export const SearchResultsPage = ({ blok }) => {
   
   return (
